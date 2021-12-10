23917d91e5b9333a99910e3e4eedc6994aa6a7e2
BradT-ITT <82958666+BradT-ITT@users.noreply.github.com>
2021-12-10 14:17:51 -0700

    Add files via upload
    I've added the files created by the spring initializr.

---
+++ Pong-Warden/src/main/java/com/QAuthProject/Pong/Warden/PongWardenApplication.java
@@ -0,0 +1,13 @@
+package com.QAuthProject.Pong.Warden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongWardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongWardenApplication.class, args);
+	}
+
+}
